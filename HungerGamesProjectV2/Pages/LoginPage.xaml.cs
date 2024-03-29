2e2b3bdc113ab221f796cabf61f01ba09c959d9b
022125 <022125@1220-001-22>
2024-03-29 14:46:01 +0300

    REGandAuth

--- HungerGamesProjectV2/Pages/LoginPage.xaml.cs
+++ HungerGamesProjectV2/Pages/LoginPage.xaml.cs
@@ -38,7 +38,18 @@ namespace HungerGamesProject.Pages
         {
             if (FIeldsHelper.CheckTwoFields(LoginTB.Text,PasswordTB.Password))
             {
-                //NavigationHelper.NavigateAfterLogin();
+                if(App.db.Users.Any(x=>x.login == LoginTB.Text && x.password == PasswordTB.Password))
+                {
+                    if (App.db.Users.Any(x=>x.login == LoginTB.Text && x.role == 1))
+                        {NavigationHelper.NavigateAfterLogin(true);}
+                    else { NavigationHelper.NavigateAfterLogin(false);}
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Такого пользователя не существует");
+                }
+
             }
             else
             {
@@ -46,7 +57,7 @@ namespace HungerGamesProject.Pages
             }
         }
 
-        private void RegBtn_SourceUpdated(object sender, DataTransferEventArgs e)
+        private void LoginTB_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
