2e44cc9f51699821f9f348a91da88f2673e31beb
muxomor <7baltika9@gmail.com>
2024-03-29 14:38:01 +0300

    basic vote adding

--- HungerGamesProjectV2/Pages/CreateVote.xaml.cs
+++ HungerGamesProjectV2/Pages/CreateVote.xaml.cs
@@ -25,9 +25,26 @@ namespace HungerGamesProjectV2.Pages
         public CreateVote()
         {
             InitializeComponent();
-            //EventNameCB.ItemsSource = App.db.Hunger_Events.ToArray();
+            EventNameCB.ItemsSource = App.db.Hunger_Events.ToArray();
            // Users[] players = App.db.Votes.Select(x => x.Players.Users.name).ToArray();
             player1CB.ItemsSource = App.db.Players.ToArray().Select(x => x.Users.login);
+            player2CB.ItemsSource = App.db.Players.ToArray().Select(x => x.Users.login);
+        }
+
+        private void CreateVoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string player1_login = player1CB.SelectedValue.ToString();
+            int player1_id =Convert.ToInt32( App.db.Players.Where(x => x.Users.login == player1_login));
+            int player2_id = Convert.ToInt32(App.db.Players.Where(x => x.Users.login == player2CB.SelectedValue.ToString()));
+            Votes vote = new Votes();
+            vote.player1_id = player1_id;
+            vote.player2_id = player2_id;
+            vote.player1_votes_qnt = 0;
+            vote.player2_votes_qnt = 0;
+            vote.vote_title = TitleTb.Text;
+            App.db.Votes.Add(vote);
+            App.db.SaveChanges();
+            
         }
     }
 }
